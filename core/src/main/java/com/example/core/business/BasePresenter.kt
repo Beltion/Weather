c6468169ba7f88cd9c440b4015467747e3e3613c
Balfagor <olegbeltion@gmail.com>
2020-12-10 22:55:52 +0300

    Add Clean Architecture base view/presenter

---
+++ core/src/main/java/com/example/core/business/BasePresenter.kt
@@ -0,0 +1,5 @@
+package com.example.core.business
+
+interface BasePresenter {
+    fun onViewCreated()
+}
---
+++ core/src/main/java/com/example/core/business/BaseView.kt
@@ -0,0 +1,6 @@
+package com.example.core.business
+
+interface BaseView {
+    fun showContent()
+    fun hideContent()
+}
