9f48739ff73380c886c42a94ff9b244c83358482
Balfagor <olegbeltion@gmail.com>
2020-12-28 02:27:09 +0300

    refactor city_list_item.xml(list of today city weather)

--- app/src/main/java/com/example/weather/business/adapters/CityListAdapter.kt
+++ app/src/main/java/com/example/weather/business/adapters/CityListAdapter.kt
@@ -3,10 +3,12 @@ package com.example.weather.business.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.core.business.entities.CityWeather
 import com.example.weather.R
+import com.squareup.picasso.Picasso
 
 class CityListAdapter(
         private val cityWeatherList: ArrayList<CityWeather>,
@@ -15,15 +17,12 @@ class CityListAdapter(
 
     class CityWeatherVH(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val name = itemView.findViewById<TextView>(R.id.name_city_list_item)
-        val main = itemView.findViewById<TextView>(R.id.main_city_list_item)
         val desc = itemView.findViewById<TextView>(R.id.desc_city_list_item)
-//        val icon = itemView.findViewById<TextView>(R.id.icon_city_list_item)
+        val icon = itemView.findViewById<ImageView>(R.id.img_city_list_item)
         val temp = itemView.findViewById<TextView>(R.id.temp_city_list_item)
-        val feelsLike = itemView.findViewById<TextView>(R.id.feels_like_city_list_item)
         val pressure = itemView.findViewById<TextView>(R.id.pressure_city_list_item)
         val humidity = itemView.findViewById<TextView>(R.id.humidity_city_list_item)
         val windSpeed = itemView.findViewById<TextView>(R.id.wind_speed_city_list_item)
-        val windDirection = itemView.findViewById<TextView>(R.id.wind_direction_city_list_item)
         val clouds = itemView.findViewById<TextView>(R.id.clouds_city_list_item)
 
         fun onClick(cW: CityWeather, listener: OnCityListClickListener){
@@ -58,15 +57,18 @@ class CityListAdapter(
     override fun onBindViewHolder(holder: CityWeatherVH, position: Int) {
         cityWeatherList[position].let{cityWeather ->
             holder.name.text = cityWeather.cityName
-            holder.main.text = cityWeather.main
             holder.desc.text = cityWeather.desc
-            holder.temp.text = cityWeather.temp.toString()
-            holder.feelsLike.text = cityWeather.feelsLike.toString()
-            holder.pressure.text = cityWeather.pressure.toString()
-            holder.humidity.text = cityWeather.humidity.toString()
-            holder.windSpeed.text = cityWeather.windSpeed.toString()
-            holder.windDirection.text = cityWeather.windDirection.toString()
-            holder.clouds.text = cityWeather.clouds.toString()
+            val temp: Int = cityWeather.temp.toInt() - 273
+            holder.temp.text = "$temp ℃"
+            holder.pressure.text = "давление " + cityWeather.pressure.toString()
+            holder.humidity.text = "влажность " + cityWeather.humidity.toString() + "%"
+            holder.windSpeed.text = "скорость ветра " + cityWeather.windSpeed.toString() + " м/с"
+            holder.clouds.text = "облачность " + cityWeather.clouds.toString() + "%"
+
+            Picasso.get()
+                .load("http://openweathermap.org/img/wn/${cityWeather.icon}@2x.png")
+                .fit()
+                .into(holder.icon)
 
             holder.onClick(
                     cityWeather,
